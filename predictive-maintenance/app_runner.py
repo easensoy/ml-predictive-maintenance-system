cabfdd856622b75260c3a793b8a935922f566fac
easensoy <eminalpersensoy@gmail.com>
2025-08-13 02:31:25 +0100

    Instantiate and run PredictiveMaintenanceAPI in app_runner.py without main function

--- predictive-maintenance/app_runner.py
+++ predictive-maintenance/app_runner.py
@@ -1,1 +1,6 @@
-from run_api import app
+
+from run_api import PredictiveMaintenanceAPI
+
+api = PredictiveMaintenanceAPI()
+api.register_additional_routes()
+api.app.run(debug=True, host='0.0.0.0', port=5000)
